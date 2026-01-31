d992f0d4e99f0007d5f80dfd729022181f34eda6
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2026-01-31 21:08:26 +0530

    update sever file

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
+// require("dotenv").config();
+// const http = require("http");
+// const { Server } = require("socket.io");
+
+// const app = require("./src/app");
+// const connectDB = require("./src/config/db");
+
+// connectDB();
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: { origin: "*" },
+// });
+
+// // make io accessible in controllers
+// app.use((req, res, next) => {
+//   req.io = io;
+//   next();
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("Client connected:", socket.id);
+// });
+
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () =>
+//   console.log(`Server running on port ${PORT}`)
+// );
 require("dotenv").config();
 const http = require("http");
 const { Server } = require("socket.io");
@@ -10,7 +39,14 @@ connectDB();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*" },
+  cors: {
+    origin: [
+      "https://order-management-frontend-beta.vercel.app",
+      "https://order-management-backend-if9a.onrender.com",
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
 // make io accessible in controllers
@@ -21,9 +57,14 @@ app.use((req, res, next) => {
 
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
